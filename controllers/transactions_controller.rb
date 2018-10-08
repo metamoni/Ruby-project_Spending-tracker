c38cbdcdbf617f2d7df4c572dd5274bb1bc5b098
Monica Mateiu <monica.mateiu@gmail.com>
2018-10-08 17:17:38 +0100

    add indexes for all classes

---
+++ controllers/transactions_controller.rb
@@ -0,0 +1,9 @@
+require("sinatra")
+require("sinatra/contrib/all")
+require_relative("../models/transaction.rb")
+also_reload("../models/*")
+
+get '/transactions' do
+  @transactions = Transaction.all()
+  erb(:'transactions/index')
+end
