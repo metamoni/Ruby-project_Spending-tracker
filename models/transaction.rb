8a5aa0d6695339e8de2ee2ea8330e4d3010bc1dd
Monica Mateiu <monica.mateiu@gmail.com>
2018-10-14 20:57:43 +0100

    add ruby functions to filter by tag, by value asc/desc

--- models/transaction.rb
+++ models/transaction.rb
@@ -27,6 +27,13 @@ class Transaction
   end
 
 
+  def self.find(id)
+    sql = "SELECT * FROM transactions WHERE id = $1;"
+    transaction = SqlRunner.run(sql, [id]).first
+    return Transaction.new(transaction)
+  end
+
+
   def update()
     sql = "UPDATE transactions SET (value, merchant_id, tag_id) = ($1, $2, $3) WHERE id = $4;"
     SqlRunner.run(sql, [@value, @merchant_id, @tag_id, @id])
@@ -45,13 +52,6 @@ class Transaction
   end
 
 
-  def self.find(id)
-    sql = "SELECT * FROM transactions WHERE id = $1;"
-    result = SqlRunner.run(sql, [id]).first
-    return Transaction.new(result)
-  end
-
-
   def tag()
     sql = "SELECT * FROM tags WHERE id = $1;"
     results = SqlRunner.run(sql, [@tag_id])
@@ -66,9 +66,25 @@ class Transaction
   end
 
 
-  def self.filter_by_tag(tag)
-    sql = "SELECT * FROM transactions WHERE tag_id = $1;"
-    transactions = SqlRunner.run(sql, [tag])
+  def self.filter_by_tag(tag_id)
+    sql = "SELECT transactions.* FROM transactions
+      INNER JOIN tags ON transactions.tag_id = tags.id
+      WHERE tags.id = $1;"
+    transactions = SqlRunner.run(sql, [tag_id])
+    return map(transactions)
+  end
+
+
+  def self.display_by_value_descending()
+    sql = "SELECT * FROM transactions ORDER BY value DESC;"
+    transactions = SqlRunner.run(sql)
+    return map(transactions)
+  end
+
+
+  def self.display_by_value_ascending()
+    sql = "SELECT * FROM transactions ORDER BY value ASC;"
+    transactions = SqlRunner.run(sql)
     return map(transactions)
   end
 
